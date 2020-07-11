66057462528b14288d01fc575bce9f409862c540
mateuseduardomedeiros <eduardomedeirosrn2@gmail.com>
2020-07-11 11:22:53 -0300

    v1 | adição de proteção nas rotas create, update e destroy| 11/07/2020

--- src/controllers/RegistroController.js
+++ src/controllers/RegistroController.js
@@ -15,32 +15,39 @@ module.exports = {
             const registro = await Registro.findOne().sort({ createdAt: -1 }).limit(1);
             return res.status(200).json(registro);
         } catch (err) {
-            return res.status(500).json({message: err.message});
+            return res.status(500).json({ message: err.message });
         }
     },
     async create(req, res) {
-        const data = req.body;
-        const schema = Joi.object().keys({
-            data: Joi.string().required(),
-            notificados: Joi.number().required(),
-            suspeitos: Joi.number().required(),
-            confirmados: Joi.number().required(),
-            internados: Joi.number().required(),
-            mortes: Joi.number().required(),
-            curados: Joi.number().required(),
-            descartados: Joi.number().required(),
-        });
-        Joi.validate(data, schema, { abortEarly: true }, async (err, value) => {
-            if (err) {
-                return res.status(500).json({ error: err.message });
-            }
-            try {
-                const registro = await Registro.create(req.body);
-                return res.status(201).send(registro);
-            } catch (err) {
-                return res.status(500).json({ error: err.message })
-            }
-        });
+        var data = req.body;
+        if (data.senha != 'ZWR1YXJkbwo=') {
+            return res.status(403).json({ message: 'Operação não permitida' })
+        } else {
+            delete data.senha;
+            const schema = Joi.object().keys({
+                data: Joi.string().required(),
+                notificados: Joi.number().required(),
+                suspeitos: Joi.number().required(),
+                confirmados: Joi.number().required(),
+                internados: Joi.number().required(),
+                mortes: Joi.number().required(),
+                curados: Joi.number().required(),
+                descartados: Joi.number().required(),
+            });
+            Joi.validate(data, schema, { abortEarly: true }, async (err, value) => {
+                if (err) {
+                    return res.status(500).json({ error: err.message });
+                }
+                try {
+                    const registro = await Registro.create(req.body);
+                    return res.status(201).send(registro);
+                } catch (err) {
+                    return res.status(500).json({ error: err.message })
+                }
+            });
+
+        }
+
     },
     async show(req, res) {
         try {
@@ -54,27 +61,42 @@ module.exports = {
         }
     },
     async update(req, res) {
-        try {
-            const registro = await Registro.findByIdAndUpdate(req.params.id, req.body, { new: true });
-            await registro.save();
-            return res.status(200).json({ message: "Registro atualizado com sucesso!", registro });
-        } catch (err) {
-            return res.status(500).json({ message: err.message });
+        var data = req.body
+        if (data.senha != 'ZWR1YXJkbwo=') {
+            return res.status(403).json({ message: 'Operação não permitida' })
+        } else {
+            delete data.senha;
+            try {
+                const registro = await Registro.findByIdAndUpdate(req.params.id, data, { new: true });
+                await registro.save();
+                return res.status(200).json({ message: "Registro atualizado com sucesso!", registro });
+            } catch (err) {
+                return res.status(500).json({ message: err.message });
+            }
         }
+
     },
     async destroy(req, res) {
-        try {
-            const registro = await Registro.findById(req.params.id);
-            if (!registro) {
-                return res.status(404).send({
-                    message: 'Registro não encontrado',
-                });
+        var data = req.body;
+        if (data.senha != 'ZWR1YXJkbwo=') {
+            return res.status(403).json({ message: 'Operação não permitida' })
+        } else {
+            delete data.senha;
+            try {
+                const registro = await Registro.findById(req.params.id);
+                if (!registro) {
+                    return res.status(404).send({
+                        message: 'Registro não encontrado',
+                    });
+                }
+                await registro.remove();
+                return res.status(200).json({ message: "Registro removido com sucesso!" });
+            } catch (err) {
+                return res.status(500).json({ message: err.message });
             }
-            await registro.remove();
-            return res.status(200).json({ message: "Registro removido com sucesso!" });
-        } catch (err) {
-            return res.status(500).json({ message: err.message });
+
         }
+
     }
 
 }
